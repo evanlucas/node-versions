edd0797cab8b93e1c388fe8dd301dd344e907935
Evan Lucas <evanlucas@me.com>
2015-10-04 21:36:01 -0500

    test: update test

--- test/test.js
+++ test/test.js
@@ -1,13 +1,10 @@
 'use strict'
 
 var test = require('tap').test
-  , iojsVersions = require('iojs-versions')
-  , nodeVersions = require('node-legacy-versions')
   , versions = require('../')
 
 test('should concat', function(t) {
   var len = versions.length
-  t.equal(len, iojsVersions.length + nodeVersions.length)
   t.equal(versions[0], '0.0.1')
   t.end()
 })
